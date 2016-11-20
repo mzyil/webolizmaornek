81a601e52943bb89c52a9a68458cd139c8752d1e
Melih YILDIZ <mzyil12@gmail.com>
2016-11-20 04:21:41 +0300

    a

--- resources/views/layouts/plane.blade.php
+++ resources/views/layouts/plane.blade.php
@@ -1,21 +1,50 @@
 <!DOCTYPE html>
-<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
-<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
-<!--[if !IE]><!-->
-<html lang="en" class="no-js">
-<!--<![endif]-->
-<head>
-	<meta charset="utf-8"/>
-	<title>SB Admin v2.0 in Laravel 5</title>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta content="width=device-width, initial-scale=1" name="viewport"/>
-	<meta content="" name="description"/>
-	<meta content="" name="author"/>
+<html lang="en">
 
-	<link rel="stylesheet" href="{{ asset("assets/stylesheets/styles.css") }}" />
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>SB Admin 2 - Bootstrap Admin Theme</title>
+
+	<!-- Bootstrap Core CSS -->
+	<link href="{{ asset("resources/css/bootstrap.css") }}" rel="stylesheet">
+
+	<!-- MetisMenu CSS -->
+	<link href="{{ asset("resources/css/metisMenu.css") }}" rel="stylesheet">
+
+	<!-- Custom CSS -->
+	<link href="{{ asset("resources/css/sb-admin-2.css") }}" rel="stylesheet">
+
+	<!-- Custom Fonts -->
+	<link href="{{ asset("resources/css/font-awesome.css") }}" rel="stylesheet" type="text/css">
+	<link href="{{ asset("resources/css/timeline.css") }}" rel="stylesheet" type="text/css">
+
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
 </head>
 <body>
 	@yield('body')
-	<script src="{{ asset("assets/scripts/frontend.js") }}" type="text/javascript"></script>
+	<!-- jQuery -->
+	<script src="{{ asset("resources/js/jquery.js") }}"></script>
+
+	<!-- Bootstrap Core JavaScript -->
+	<script src="{{ asset("resources/js/bootstrap.js") }}"></script>
+
+	<!-- Metis Menu Plugin JavaScript -->
+	<script src="{{ asset("resources/js/metisMenu.js") }}"></script>
+
+	<!-- Custom Theme JavaScript -->
+	<script src="{{ asset("resources/js/sb-admin-2.js") }}"></script>
+	<script src="{{ asset("resources/js/frontend.js") }}" type="text/javascript"></script>
 </body>
 </html>
